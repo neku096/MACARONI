a6be52d8be7a7a58215da1ca0fa5e3decdab799e
neku096 <ikirito116@gmail.com>
2026-05-25 09:14:21 +0900

    Remove tracked Next.js dev build artifacts

--- .next-dev-3101/dev/static/chunks/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_0zl6c1m._.js","static/chunks/components_0g5zxxl._.js","static/chunks/_0v8t5g9._.css"],
-    source: "dynamic"
-});
